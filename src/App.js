49e068e8afbc277efce968f894f5c8e6f02c8bd0
Vlad-Otrishko <reactor.doe0@gmail.com>
2021-11-25 21:50:19 +0200

    adds bug-fixes
    fixed problem of logging-out user on page reload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Suspense, lazy } from 'react';
-import { useSelector} from 'react-redux';
+import { useEffect,Suspense, lazy } from 'react';
+import { useSelector, useDispatch} from 'react-redux';
 import { Switch} from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
+import { authOperations } from './redux/auth';
 
 import AuthNav from './components/AuthNav';
 import Navigation from './components/Navigation';
@@ -18,26 +19,31 @@ const ContactsView = lazy(() => import('./views/ContactsView'));
 
 
 const App = () => {
+  const dispatch = useDispatch();
+    useEffect(() => {
+      dispatch(authOperations.fetchCurrentUser());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
   
     return (
       <div className={s.container}>
         <Navigation />
         {useSelector(authSelectors.getIsLoggedIn) ? <UserMenu /> : <AuthNav />}
         <Switch>
-          <Suspense fallback={<Loader/>}>
+          <Suspense fallback={<Loader />}>
             <PublicRoute exact path="/">
-            <HomeView />
-          </PublicRoute>
-          <PublicRoute exact path="/register" restricted>
-            <RegisterView />
-          </PublicRoute>
-          <PublicRoute exact path="/login" redirectTo="/" restricted>
-            <LoginView />
-          </PublicRoute>
-          <PrivateRoute path="/contacts" redirectTo="/">
-            <ContactsView />
+              <HomeView />
+            </PublicRoute>
+            <PublicRoute exact path="/register" restricted>
+              <RegisterView />
+            </PublicRoute>
+            <PublicRoute exact path="/login" redirectTo="/" restricted>
+              <LoginView />
+            </PublicRoute>
+            <PrivateRoute path="/contacts" redirectTo="/">
+              <ContactsView />
             </PrivateRoute>
-         </Suspense>
+          </Suspense>
         </Switch>
       </div>
     );
--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -4,9 +4,12 @@ const getUsername = state => state.auth.user.name;
 
 const getUsermail = state => state.auth.user.email;
 
+const getToken =state=>state.auth.token
+
 const authSelectors = {
   getIsLoggedIn,
   getUsername,
   getUsermail,
+  getToken,
 };
 export default authSelectors;
